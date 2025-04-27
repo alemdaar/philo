72bd944c262320a4c694b13c21dac494ae224c8e
Oussama Elhassouni <oelhasso@c1r7p3.1337.ma>
2025-04-27 12:55:51 +0100

    still

--- header.h
+++ header.h
@@ -9,7 +9,12 @@
 // structs
 typedef struct s_info
 {
-    int philos;
+    int number_of_philosophers;
+    int time_to_die;
+    int time_to_eat;
+    int time_to_sleep;
+    int number_of_times_each_philosopher_must_eat;
+    int nb_of_inputs;
 } t_info;
 
 // prototypes
--- src/parcing/parcing.c
+++ src/parcing/parcing.c
@@ -16,28 +16,36 @@ int is_number(char *str)
     }
     return 1;
 }
-void    check_input(int ac, char **av, t_info *info)
+
+void set_up(char av, t_info *dainfo)
 {
-    int i;
-    int r;
-    i = 1;
-    int nb[ac - 1];
-    while (i < ac)
-    {
-        r = is_number(av[i]);
-        if (r == 0)
-            why_exit("the inpit is not a number\n", 1);
-        nb[i] = myatoia(av[i]);
-        if (nb[i] == 0)
-            why_exit("one of the inputs is 0\n", 1);
-        i++;
-    }
-    if (nb[0] > 200)
-        why_exit("the number of philos is bigger than 200\n", 1);
-    
+
 }
 
-int parcing(int ac, char **av, t_info *dainfo)
+void    check_input(int ac, char **av, t_info *dainfo)
 {
-    check_input(av, ac);
+	int i;
+	int r;
+	i = 1;
+	int nb[ac - 1];
+	while (i < ac - 1)
+	{
+		r = is_number(av[i]);
+		if (r == 0)
+		    why_exit("the inpit is not a number\n", 1);
+		nb[i] = myatoia(av[i]);
+		if (nb[i] == 0 && i != 5)
+		    why_exit("one of the inputs is 0\n", 1);
+		i++;
+	}
+	if (ac == 5)
+		dainfo->number_of_times_each_philosopher_must_eat = -1;
+	dainfo->nb_of_inputs = ac - 1;
+	set_up(av, dainfo, nb);
+	return;
+}
+
+void	parcing(int ac, char **av, t_info *dainfo)
+{
+	check_input(av, ac, dainfo);
 }
